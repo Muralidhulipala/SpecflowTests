71b281be2287c6ecdccd9c507096a6a2947f30b4
Muralidhulipala <murali645d@gmail.com>
2022-12-21 19:40:49 +0530

    Update DriverFactory.cs

--- SpecflowExamples/Framework/DriverFactory.cs
+++ SpecflowExamples/Framework/DriverFactory.cs
@@ -17,7 +17,7 @@ namespace SpecflowExamples.Framework
         [ThreadStatic]
         public static RemoteWebDriver? driver;
        
-        public static string remote_url_chrome = "http://127.0.0.1:4444/wd/hub";
+        public static string remote_url_chrome = "http://127.0.0.1:4444";
         
         public static void InIt(string Browsername)
         {
